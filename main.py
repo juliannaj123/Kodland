cbb402618b92bdb51b2edf13382d0c9f2c7c9e7f
juliannaj123 <159059490+juliannaj123@users.noreply.github.com>
2024-02-26 19:08:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
-meme_dict = {
-            "CRINGE - Coś wyjątkowo dziwnego lub zawstydzającego",
-            "LOL - Częsta reakcja na coś zabawnego",
-            "ROFL - odpowiedź na żart",
-            "SHEESH - lekka dezaprobata",
-            "CREEPY - straszny, złowieszczy",
-            "AGGRO - stać się agresywnym/zły"
-            }
-while True:
-    for word in range:
-        word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
-        if word in meme_dict():
-            print (x[y])
-    # Co powinniśmy zrobić, jeśli słowo zostało znalezione?
-else:
-    print('Nie mam yjeszcze tego słowa :(')
-    # Co powinniśmy zrobić, jeśli słowo nie zostało znalezione?
+import discord
+import random
+from discord.ext import commands
+from bot_logic import rpsc, random_number
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='&', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def repeat(ctx, count_repeat = 5):
+    n= " "
+    await ctx.send(n= input('Jakie słowo powtórzyć?'))
+    await ctx.send(x * count_repeat)
+
+@bot.command()
+async def goodbye(ctx):
+    await ctx.send(f'Do widzenia!')
+
+@bot.command()
+async def rockpaperscissors(ctx):
+    await ctx.send(rpsc())
+
+@bot.command()
+async def randomnumber(ctx):
+    await ctx.send(random_number())
+
+bot.run("MTIwOTE4Njg5NjAxODkzOTkxNg.GlE_px.xw-0raYOX82np_pwzSkb7536EudhRbROZ-zFI8")
